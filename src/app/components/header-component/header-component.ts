d1ae49b8391fdac8c9f154fb1bb5ffdddaa22a63
David Roitinger <david.roitinger@gmail.com>
2026-03-17 00:26:52 +0100

    Fix Navbar No. 34

--- src/app/components/header-component/header-component.ts
+++ src/app/components/header-component/header-component.ts
@@ -19,9 +19,11 @@ export class HeaderComponent  {
 
   navigateTo(path: string) : void {
 
-    document.getElementById('offcanvasNavbar')?.classList.remove('show');
-    document.getElementById('offcanvasNavbar')?.classList.add('hiding');
+    // document.getElementById('offcanvasNavbar')?.classList.remove('show');
+    // document.getElementById('offcanvasNavbar')?.classList.add('hiding');
     try{
+      //@ts-ignore
+      document.getElementsByClassName('navbar-toggler')[0].click();
       document.getElementsByClassName('offcanvas-backdrop')[0].remove();
     } catch {
       console.log("tim stinkt?")
